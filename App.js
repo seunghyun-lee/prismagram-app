a5655e619afcda0451a3b808e2ec6d9fb8e2a158
Seunghyun <seunghyunleekr@gmail.com>
2020-01-19 19:57:31 +0900

    preloading assets and cache & auth

--- App.js
+++ App.js
@@ -1,19 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { AsyncStorage } from "react-native";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloCilentOptions from "./apollo";
+import styles from "./styles";
+import NavController from "./components/NavController";
+import { AuthProvider} from "./AuthContext";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  const [loaded, setLoaded] =  useState(false);
+  const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloCilentOptions
+      });
+      // const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      // if (isLoggedIn === null || isLoggedIn === "false") {
+      //   setIsLoggedIn(false);
+      // } else {
+      //   setIsLoggedIn(true);
+      // }
+      setLoaded(true);
+      setClient(client);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  // const logUserIn = async () => {
+  //   try {
+  //     await AsyncStorage.setItem("isLoggedIn", "true");
+  //     setIsLoggedIn(true);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // const logUserOut = async () => {
+  //   try {
+  //     await AsyncStorage.setItem("isLoggedIn", "false");
+  //     setIsLoggedIn(false);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={styles}>
+      {/* <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          {isLoggedIn === true ? (
+            <TouchableOpacity onPress={logUserOut}>
+              <Text>Log Out</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={logUserIn}>
+              <Text>Log In</Text>
+            </TouchableOpacity>
+          )}
+        </View> */}
+
+        <AuthProvider>
+          <NavController />
+        </AuthProvider>
+      </ThemeProvider>
+    </ApolloProvider>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
