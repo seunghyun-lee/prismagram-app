42a839b7d674d38612c3bb70277f47b64dc35b49
seunghyun <seunghyunleekr@gmail.com>
2020-01-23 17:26:44 +0900

    add tab icons

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { Platform, View, Text, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-import { createStackNavigator } from "react-navigation-stack";
+import { createStackNavigator, HeaderTitle } from "react-navigation-stack";
 import Home from "../screens/Tabs/Home";
 import Search from "../screens/Tabs/Search";
 import Notifications from "../screens/Tabs/Notifications";
 import Profile from "../screens/Tabs/Profile";
 import MessagesLink from "../components/MessagesLink";
+import NavIcon from "../components/NavIcon";
 
 const stackFactory = (initialRoute, customConfig) => 
     createStackNavigator(
@@ -18,32 +19,62 @@ const stackFactory = (initialRoute, customConfig) =>
         }
     );
 
-export default createBottomTabNavigator({    
-    Home: {
-        screen: stackFactory(Home, {
-            title: "Home",
-            headerRight: () => <MessagesLink />            
-        })
-    },
-    Search: {
-        screen: stackFactory(Search, {
-            title: "Search"
-        })
-    },
-    Add: {
-        screen: View,
-        navigationOptions: {
-            tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+export default createBottomTabNavigator(
+    {    
+        Home: {
+            screen: stackFactory(Home, {
+                headerRight: () => <MessagesLink />,
+                headerTitle: () => <NavIcon name="logo-instagram" size={36} />
+            }),
+            navigationOptions: {
+                tabBarIcon: (
+                    <NavIcon name={Platform.OS === "ios" ? "ios-home" : "md-home"} />
+                )
+            }
+        },
+        Search: {
+            screen: stackFactory(Search, {
+                title: "Search"
+            }),
+            navigationOptions: {
+                tabBarIcon: (
+                    <NavIcon name={Platform.OS === "ios" ? "ios-search" : "md-search"} />
+                )
+            }
+        },
+        Add: {
+            screen: View,
+            navigationOptions: {
+                tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation"),
+                tabBarIcon: (
+                    <NavIcon name={Platform.OS === "ios" ? "ios-add" : "md-add"} />
+                )
+            }
+        },
+        Notifications: {
+            screen: stackFactory(Notifications, {
+                title: "Notifications"
+            }),
+            navigationOptions: {
+                tabBarIcon: (
+                    <NavIcon name={Platform.OS === "ios" ? "ios-heart" : "md-heart"} />
+                )
+            }
+        },
+        Profile: {
+            screen: stackFactory(Profile, {
+                title: "Profile"
+            }),
+            navigationOptions: {
+                tabBarIcon: (
+                    <NavIcon name={Platform.OS === "ios" ? "ios-person" : "md-person"} />
+                )
+            }
         }
     },
-    Notifications: {
-        screen: stackFactory(Notifications, {
-            title: "Notifications"
-        })
-    },
-    Profile: {
-        screen: stackFactory(Profile, {
-            title: "Profile"
-        })
-    }
-});
+    {
+        tabBarOptions: {
+            showLabel: false
+        }
+    }    
+);
